166449d842fa4c29f1a05c75374ba9f2cde35d28
Rajeshkannan Palvannan <rajeshkannan.p@themesoft.com>
2016-10-14 19:04:35 +0530

    Test Commit

--- test-file-upload.js
+++ test-file-upload.js
@@ -2,17 +2,4 @@
 
 /* Test comment */
 
-var app = angular.module('Directives', ['htmlSortable']);
-
-app.controller('MainCtrl', function($scope) {
-  $scope.data1 = [
-    { id: 1, name: 'A' },
-    { id: 2, name: 'B' },
-    { id: 3, name: 'C' },
-    { id: 4, name: 'D' }
-  ];
-
-  $scope.add = function () {
-    $scope.data1.push({id: $scope.data1.length + 1, name: 'E'});
-  };
-});
+test
